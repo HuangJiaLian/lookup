ad6e8e314b8e2349fab8c8d71e7109eaeb6d9ae7
Jack Huang <719668276@qq.com>
2018-09-18 17:20:03 +0800

    The first commit

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# lookup
-英文文献阅读神器
+# 查词帮
+一个更方便的查词工具
+
+## 依赖
+```
+## 1. Translate Shell 
+https://github.com/soimort/translate-shell
+## 2. pyperclip
+https://pyperclip.readthedocs.io/en/latest/introduction.html
+## 3. notify-send
+sudo apt install libnotify-bin
+```
+
+
+
+## 使用
+
+``` bash
+python lookup.py
+```
+
+将单词复制到剪贴板，即可使用。
---
+++ lookup.py
@@ -0,0 +1,32 @@
+import os,sys,string
+import time,signal
+import pyperclip
+
+
+# Interupt Handler
+is_sigint_up = False
+def sigint_handler(signum, frame):
+ 	global is_sigint_up
+ 	is_sigint_up = True
+ 	print ('Existing ...')
+signal.signal(signal.SIGINT, sigint_handler)
+
+def welcom():
+	print("+----------------------+")
+	print("|       查词帮         |")
+	print("|  一个更方便的查词工具|") 
+	print("+----------------------+")
+
+welcom()
+lookup_cmd = "trans -sp  :zh info_to_lookup"
+notify_cmd = "notify-send -i face-laugh TITLE \"`trans -p  -brief :zh info_to_lookup`\""
+recent_value = pyperclip.paste()
+while not is_sigint_up:
+	tmp_value = pyperclip.paste()
+	if tmp_value != recent_value:
+		recent_value = tmp_value
+		cmd_0 = lookup_cmd.replace('info_to_lookup',recent_value)
+		cmd_1 = notify_cmd.replace('info_to_lookup', recent_value).replace('TITLE',recent_value)
+		os.system(cmd_0)
+		os.system(cmd_1)
+	time.sleep(0.1)
