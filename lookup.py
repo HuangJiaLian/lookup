ef99a67742852fe88724acf296954b7f83119eee
Jack Huang <719668276@qq.com>
2018-10-05 11:44:24 +0800

    修改输出格式，让显示更为合理

--- lookup.py
+++ lookup.py
@@ -13,22 +13,24 @@ def sigint_handler(signum, frame):
 signal.signal(signal.SIGINT, sigint_handler)
 
 def welcom():
-	print("+----------------------+")
-	print("|       查词帮         |")
-	print("|  一个更方便的查词工具|") 
-	print("+----------------------+")
-
+	print('+-------------------------------------------------+')
+	print('| Lookup, a more convenient tool for translation. |') 
+	print('+-------------------------------------------------+\n')
 welcom()
 # lookup_cmd = "trans -sp  :zh info_to_lookup"
 lookup_cmd = "trans   :zh info_to_lookup"
 notify_cmd = "notify-send -i face-glasses TITLE \"`trans  -brief :zh info_to_lookup`\""
 speak_cmd = 'echo \'info_to_speak\' | festival --tts'
+# speak_cmd = 'pico2wave -l=en-US -w=/tmp/test.wav \'info_to_speak\' && aplay /tmp/test.wav '
 primary_cmd = 'echo \'info_to_add\' | xsel'
 # notify_cmd = "notify-send -i face-glasses TITLE \"`trans -p  -brief :zh info_to_lookup`\""
 recent_value = pyperclip.paste()
+counter = 0
+print('[0]>> usage: Copy an English word to the clipboard.')
 while not is_sigint_up:
 	tmp_value = pyperclip.paste()
 	if tmp_value != recent_value:
+		counter += 1
 		recent_value = tmp_value
 		cmd_0 = lookup_cmd.replace('info_to_lookup',recent_value)
 		cmd_speak = speak_cmd.replace('info_to_speak', recent_value)
@@ -37,10 +39,8 @@ while not is_sigint_up:
 		# Print Translations to terminal 
 		translations = os.popen(cmd_0).read()
 		translations = re.findall(r'[\u4e00-\u9fff]+', translations)
-		print(translations)
+		print('['+str(counter)+']>>',recent_value, '\n',translations[2:],'\n')
 		cmd_add_to_primary = primary_cmd.replace('info_to_add',translations[0])
 		os.system(cmd_1)
 		os.system(cmd_add_to_primary)
-
-		
 	time.sleep(0.1)
